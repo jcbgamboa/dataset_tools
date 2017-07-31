1aa1dd1bf0b478e7aac4f573a8705ec4607fecc5
John Gamboa <vaulttech@gmail.com>
2017-08-01 00:12:26 +0200

    Fixes `n_gram_generate.py` + add option to capitalize n-gram words
     Changes to be committed:
    	modified:   amazon_product_data/n_gram_generate.py

--- amazon_product_data/n_gram_generate.py
+++ amazon_product_data/n_gram_generate.py
@@ -12,7 +12,8 @@ def find_ngrams(input_list, n):
 	return zip(*[input_list[i:] for i in range(n)])
 
 
-def generate_n_grams(review_text, out_src, out_tgt, nlp, n_min, n_max):
+def generate_n_grams(review_text, out_src, out_tgt, nlp,
+			n_min, n_max, capitalize):
 	doc = nlp(review_text)
 	doc_sentences = (doc.sents)
 
@@ -21,19 +22,29 @@ def generate_n_grams(review_text, out_src, out_tgt, nlp, n_min, n_max):
 		s = re.sub("'", '', i.text)
 		s = re.sub('[^a-zA-Z0-9]', ' ', s)
 		s = re.sub('\s+', ' ', s).strip()
+
+		if (capitalize == 'lowercase'):
+			s = s.lower()
+		elif (capitalize == 'title'):
+			s = s.title()
+
 		sentences.append(s)
 
-	# TODO: think well about this -- this is obviously not working
-	#for j in sentences:
-	#	for i in range(n_min, n_max+1):
-	#		n_grams_generator = find_ngrams(j, i)
-	#		for k in n_grams_generator:
-	#			#texts = [k.text for k in k]
-	#			out_src.write(''.join(k) + '\n')
-	#			out_tgt.write(' '.join(k) + '\n')
+	if len(sentences) == 1:
+		pass
 
+	for i in sentences:
+		doc = nlp(i)
+		words = list(doc)
+		for j in range(n_min, n_max+1):
+			n_grams_generator = find_ngrams(words, j)
+			for k in n_grams_generator:
+				texts = [k.text for k in k]
+				out_src.write(''.join(texts) + '\n')
+				out_tgt.write(' '.join(texts) + '\n')
 
-def run_generate_n_grams(in_file, out_file, nlp, batch_size=8192, n_min=2, n_max=6):
+def run_generate_n_grams(in_file, out_file, nlp, capitalize, batch_size=8192,
+			n_min=2, n_max=6):
 	curr_batch = 0
 
 	with open(in_file, 'r') as f, open(out_file + '.src', 'w') as g, \
@@ -51,7 +62,8 @@ def run_generate_n_grams(in_file, out_file, nlp, batch_size=8192, n_min=2, n_max
 				review_text = data['reviewText']
 
 				generate_n_grams(review_text, g, h,
-						nlp, n_min, n_max)
+						nlp, n_min, n_max,
+						capitalize)
 
 				del review_text, data
 			del next_n_lines
@@ -65,13 +77,21 @@ def parse_command_line():
 	parser.add_argument("in_file", help="input file name.")
 	parser.add_argument("out_file", help="input file name.")
 	parser.add_argument("language", help="input language ('en' or 'de').")
+
+	parser.add_argument("--capitalize",
+		help="Change capitalization of the first letter of each " +
+			"word in the n-gram. If 'lowercase', then all words in " +
+			"each n-gram are lowercased; if 'title', then each word" +
+			"in each n-gram has its first letter capital",
+		default='none')
 	return parser.parse_args()
 
 def main():
 	args = parse_command_line()
 
 	nlp = spacy.load(args.language)
-	run_generate_n_grams(args.in_file, args.out_file, nlp)
+	run_generate_n_grams(args.in_file, args.out_file, nlp,
+				args.capitalize)
 
 if __name__ == '__main__':
 	main()
